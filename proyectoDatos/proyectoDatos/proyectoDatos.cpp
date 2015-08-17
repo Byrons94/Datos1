5df0e084747bb3c975db9c3e7e3880d40c51972f
Byrons94 <bironjosue@hotmail.com>
2015-08-17 00:27:09 -0600

    articulos en proceso...

--- proyectoDatos/proyectoDatos/proyectoDatos.cpp
+++ proyectoDatos/proyectoDatos/proyectoDatos.cpp
@@ -9,11 +9,13 @@
 #include<string.h>
 #include "ListaArticulo.h"
 
-int main()
-{
+int main(){
 	GestorInventario *a = new GestorInventario();
 	
 	ListaArticulo *lista = a->cargarArticulos();
+	
+	a->crearProducto("3", "Condones", "Prudence", 1, 1275, lista);
+
 	NodoArticulo *aux = lista->getCab();
 	while (aux != NULL){
 		cout << "Codigo: " << aux->getInfo()->getCodigo() << endl;
@@ -22,6 +24,7 @@ int main()
 		cout << "Tamanio: " << aux->getInfo()->getTamanio() << endl;
 		cout << "Precio: " << aux->getInfo()->getPrecio() << endl;
 		aux = aux->getSgte();
+		cout << endl;
 	}
 
 	system("PAUSE");
