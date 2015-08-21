db279e9d20112663e63e17e5d0908fd2c5826504
Byrons94 <bironjosue@hotmail.com>
2015-08-20 20:44:11 -0600

    Carrito compras

--- proyectoDatos/proyectoDatos/ListaCarrito.h
+++ proyectoDatos/proyectoDatos/ListaCarrito.h
@@ -3,19 +3,57 @@
 class ListaCarrito
 {
 private:
+	//atributos
 	int tamanio;
 	NodoCarrito * cab;
 
+	//metodos privados
+	NodoCarrito * dirNodo(char *);
+	char * dirCodigoDeNodo(NodoCarrito* nodo);
+	NodoCarrito * dirUltimo();
+	NodoCarrito * dirAnterior(char *);
+	NodoCarrito * dirIndex(int);
+	void agregarNodoDespuesDe(NodoCarrito *, NodoCarrito*);
+	void agregarNodoAntesDe(NodoCarrito *, NodoCarrito*);
+	void borrar(NodoCarrito * );
 
 
 public:
+	//constructores
 	ListaCarrito();
+	ListaCarrito(NodoCarrito *);
 	~ListaCarrito();
 
+	//metodos accesores de la lista
 	void setTamanio(int);
-	void setCab(NodoCarrito);
+	void setCab(NodoCarrito*);
 	int getTamanio();
-	NodoCarrito getCab();
+	NodoCarrito *getCab();
+	
+	void insetarInicio(InfoCarrito *);
+	void insertarFinal(InfoCarrito*);
+	bool insertarAntesDe(InfoCarrito*, char *);
+	bool insertarDespuesDe(InfoCarrito*, char *);
+	bool insertarAcendente(InfoCarrito*);
+	bool insertarDecendente(InfoCarrito*);
+	bool insertarIndex(InfoCarrito*, int);
+
+
+
+	bool elimina(char *);
+	bool eliminarIndex(int);
+	void vaciar();
+
+	bool estaVacia();
+
+	InfoCarrito* obtenerDato(char *);
+	InfoCarrito* obtenerPorIndex(int);
+
+	bool modificarDatos(char *, InfoCarrito *);
+	bool modificarDatoPorIndex(int, InfoCarrito*);
+
+	void mostrarLista();
+
 
 
 };
