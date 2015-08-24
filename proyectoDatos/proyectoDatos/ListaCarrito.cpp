c29e6f8dffc9f7ad53c9a32ce0a9580b8d1445a5
Byrons94 <bironjosue@hotmail.com>
2015-08-24 13:48:14 -0600

    toal

--- proyectoDatos/proyectoDatos/ListaCarrito.cpp
+++ proyectoDatos/proyectoDatos/ListaCarrito.cpp
@@ -411,26 +411,7 @@ int ListaCarrito::leerFicheroCarritoPendientes() {
 }
 
 
-				aux << carrito->getCodigo() << "; " << carrito->getCodClie() << "; "
-					<< carrito->getNombre() << "; " << carrito->getEstado() << "; "
-					<< carrito->getMonto() << "; " << std::endl;
-				
-			}
-			else {
-				aux << codigo << "; " << codCliente << "; "
-					<< nombre << "; " << estado << "; "
-					<< monto << "; " << std::endl;
-			}
-			lectura >> estado;
-		}
-	}
-	
-	aux.close();
-	lectura.close();
-	remove("Ficheros/carritos.txt");
-	rename("Ficheros/auxiliar.txt", "Ficheros/carritos.txt");
-}
-
+		
 
 void ListaCarrito::convertirAChar(char *palabra, std::string palabraString) {
 	palabraString.erase(palabraString.find(' '), 1); //elimina los espacios en blanco que se hacen al principio
